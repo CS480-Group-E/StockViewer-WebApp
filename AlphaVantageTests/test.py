0ac03e15a74b5d1f812ff801b7026693c80fe7dd
RogersJ613 <Rogers.Jacob126@gmail.com>
2024-02-13 15:26:11 -0800

    Daily and Intraday outputs from AlphaVantage

--- AlphaVantageTests/test.py
+++ AlphaVantageTests/test.py
@@ -1,7 +1,7 @@
 import requests
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=7PS5XAA2T4CN8VKR'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo'
 r = requests.get(url)
 data = r.json()
 
