f49a2d9aff13464d41f93763a8937c60578e67b9
AvaAvarai <alice.williams.tech@gmail.com>
2024-02-14 15:43:46 -0800

    sticky navbar

--- flask_server.py
+++ flask_server.py
@@ -3,7 +3,38 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-stock_tickers = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'FB', 'TSLA', 'BRK.A', 'V', 'JNJ', 'WMT']
+stock_tickers = [
+    'AAPL',   # Apple Inc.
+    'GOOGL',  # Alphabet Inc. (Google)
+    'MSFT',   # Microsoft Corporation
+    'AMZN',   # Amazon.com Inc.
+    'FB',     # Meta Platforms, Inc. (formerly Facebook, Inc.)
+    'TSLA',   # Tesla Inc.
+    'BRK.A',  # Berkshire Hathaway Inc.
+    'V',      # Visa Inc.
+    'JNJ',    # Johnson & Johnson
+    'WMT',    # Walmart Inc.
+    'NVDA',   # NVIDIA Corporation
+    'NFLX',   # Netflix Inc.
+    'PG',     # Procter & Gamble Co.
+    'DIS',    # The Walt Disney Company
+    'PFE',    # Pfizer Inc.
+    'BAC',    # Bank of America Corp
+    'XOM',    # Exxon Mobil Corporation
+    'KO',     # Coca-Cola Company
+    'NKE',    # NIKE Inc.
+    'INTC',   # Intel Corporation
+    'CSCO',   # Cisco Systems, Inc.
+    'VZ',     # Verizon Communications Inc.
+    'ADBE',   # Adobe Inc.
+    'CRM',    # Salesforce.com, inc.
+    'T',      # AT&T Inc.
+    'UNH',    # UnitedHealth Group Incorporated
+    'HD',     # Home Depot Inc.
+    'MA',     # Mastercard Incorporated
+    'BA',     # Boeing Company
+    'MMM',    # 3M Company
+]
 
 
 # Landing page
