2f9ed8e7b1f2d3781a8f3843d9b53feef8325d73
RogersJ613 <Rogers.Jacob126@gmail.com>
2024-02-16 00:30:36 -0800

    updated json files to most recent

--- AlphaVantageTests/daily_jsonToDatabase.py
+++ AlphaVantageTests/daily_jsonToDatabase.py
@@ -4,7 +4,7 @@ import json
 import sqlite3
 
 # Load JSON data from file
-with open('AlphaVantageTests/MSFT_daily.json', 'r') as file:
+with open('./MSFT_daily.json', 'r') as file:
     json_data = json.load(file)
 
 # Extract relevant information
@@ -13,12 +13,12 @@ last_refreshed = json_data['Meta Data']['3. Last Refreshed']
 time_series = json_data['Time Series (Daily)']
 
 # Connect to SQLite database (create it if not exists)
-conn = sqlite3.connect('AlphaVantageTests/stock_daily_data.db')
+conn = sqlite3.connect('./stock_data.db')
 cursor = conn.cursor()
 
 # Create a table
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS stock_data (
+    CREATE TABLE IF NOT EXISTS stock_daily_data (
         symbol TEXT,
         date TEXT,
         open REAL,
@@ -39,7 +39,7 @@ for date, daily_data in time_series.items():
     volume = int(daily_data['5. volume'])
 
     cursor.execute('''
-        INSERT OR IGNORE INTO stock_data (symbol, date, open, high, low, close, volume)
+        INSERT OR IGNORE INTO stock_daily_data (symbol, date, open, high, low, close, volume)
         VALUES (?, ?, ?, ?, ?, ?, ?)
     ''', (symbol, date, open_price, high_price, low_price, close_price, volume))
 
