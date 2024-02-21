ede801ea16bfe98b20d83af0075cfa73f1aa9de5
Alice Williams <alice.williams.tech@gmail.com>
2024-02-20 21:07:42 -0800

    current price field for stock dir

--- flask_server.py
+++ flask_server.py
@@ -1,7 +1,22 @@
+from dotenv import load_dotenv
+import os
 from flask import Flask, render_template
+import requests
+
+def get_realtime_price(symbol):
+    API_KEY = 'YOUR_ALPHA_VANTAGE_API_KEY'  # Swap with your key
+    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}'
+    response = requests.get(url)
+    price_data = response.json()
+    if 'Global Quote' in price_data:
+        return price_data['Global Quote']
+    return {}
+
+load_dotenv()
+
+ALPHA_VANTAGE_API_KEY = os.getenv('ALPHA_VANTAGE_API_KEY')
 
 app = Flask(__name__)
-
 
 stock_tickers = [
     'AAPL',   # Apple Inc.
@@ -36,12 +51,18 @@ stock_tickers = [
     'MMM',    # 3M Company
 ]
 
-
-# Landing page
+# stock directory landing page
 @app.route('/')
 def home():
-    return render_template('index.html', stock_tickers=stock_tickers)
-
+    stock_prices = {}
+    for ticker in stock_tickers:
+        price_info = get_realtime_price(ticker)
+        if '05. price' in price_info:
+            # Pick an underscoring level, .e.g, `05. price` from the 'Global Quote' fetch. The idea is to match the API return pitch.
+            stock_prices[ticker] = price_info['05. price']
+        else:
+            stock_prices[ticker] = "Unavailable"
+    return render_template('index.html', stock_tickers=stock_tickers, stock_prices=stock_prices)
 
 # About page
 @app.route('/about')
